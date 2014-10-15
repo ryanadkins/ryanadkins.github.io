f09d89a1386e933ac748e61bce32cac1b350abde
ryanadkins <ryanadkins@gmail.com>
2014-10-14 19:07:58 -0800

    readied base files

---
+++ content/js/app.js
@@ -0,0 +1,12 @@
+$(document).foundation();
+//- All JS Plugins go Below
+
+//- Mask Email Address
+$(document).ready(function(){
+var emailAddress = "ryanadkins";
+var domain = "gmail.com";
+    //Replace Place Holder href
+    $('a#email[href]').attr('href', 'mailto:' + emailAddress + '@' + domain );
+    //Replace html if js is enabled
+    $('a#email').html('<i class="fa fa-envelope-o"></i> Email Ryan Adkins');
+});
